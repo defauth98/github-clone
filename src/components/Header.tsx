41b72fc481d551c08a3a6232b6d88223fd9b2b17
defauth98 <neto.daniribeiro@gmail.com>
2021-01-06 14:01:15 -0300

    Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import { AiFillGithub } from 'react-icons/ai';
+
 const Header: React.FC = () => {
   return (
-    <header className="bg-black h-14 ">
-      <input type="text" name="github-username" id="github-username" />
+    <header className="bg-gray-800 h-14 flex items-center px-2">
+      <button className="mr-4 cursor-pointer">
+        <AiFillGithub size={40} />
+      </button>
+      <input
+        type="text"
+        name="github-username"
+        id="github-username"
+        placeholder="Github Username"
+        className="w-full h-8  rounded bg-gray-700 border-gray-900 px-2 color text-gray-900	"
+      />
     </header>
   );
 };
